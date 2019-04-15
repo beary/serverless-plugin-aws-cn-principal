82c69c5410ffe9170ad243ac0ee43ed70f0dbf43
beary <bearyme@outlook.com>
2019-04-15 15:28:42 +0800

    replace before generate json files

--- index.js
+++ index.js
@@ -1,47 +1,23 @@
-const { resolve } = require('path')
-const { readFileSync, writeFileSync } = require('fs')
-
 class AWSCNPrincipal {
   constructor(serverless, options) {
     this.serverless = serverless
     this.options = options
     this.hooks = {
-      // https://github.com/serverless/serverless/blob/master/lib/plugins/aws/package/index.js#L68
-      'package:finalize': () => {
-        // https://github.com/serverless/serverless/blob/master/lib/plugins/aws/package/lib/saveCompiledTemplate.js#L10
-        const serverlessDirPath = resolve(this.serverless.config.servicePath, '.serverless')
-        const serviceStateFileName = this.serverless.providers.aws.naming.getServiceStateFileName()
-        const compiledTemplateFileName = this.serverless.providers.aws.naming.getCompiledTemplateFileName()
+      // https://gist.github.com/HyperBrain/bba5c9698e92ac693bb461c99d6cfeec#lifecycle-events-outer-events-in-bold-complete-graph-1
+      // https://github.com/serverless/serverless/blob/master/lib/plugins/deploy/deploy.js#L21
+      'package:compileEvents': () => {
+        // https://github.com/serverless/serverless/blob/master/lib/plugins/aws/package/compile/events/apiGateway/lib/permissions.js#L53
+        const rs = this.serverless.service.provider.compiledCloudFormationTemplate.Resources
 
-        const serviceStateFileNamePath = resolve(serverlessDirPath, serviceStateFileName)
-        const compiledTemplateFileNamePath = resolve(serverlessDirPath, compiledTemplateFileName)
-
-        const serviceStateFileJson = JSON.parse(readFileSync(serviceStateFileNamePath, { encoding: 'utf8' }))
-        const compiledTemplateFileJson = JSON.parse(readFileSync(compiledTemplateFileNamePath, { encoding: 'utf8' }))
-
-
-        const serviceStateResources = serviceStateFileJson.service.provider.compiledCloudFormationTemplate.Resources
-
-        Object.keys(serviceStateResources).forEach(key => {
-          const it = serviceStateResources[key]
-          if (it.Properties && it.Properties.Principal)
+        Object.keys(rs).forEach(key => {
+          const it = rs[key]
+          if (it.Properties && it.Properties.Principal) {
             it.Properties.Principal = JSON.parse(
               JSON.stringify(it.Properties.Principal)
                 .replace('{"Ref":"AWS::URLSuffix"}', '"amazonaws.com"')
             )
+          }
         })
-        writeFileSync(serviceStateFileNamePath, JSON.stringify(serviceStateFileJson, null, 2))
-
-
-        Object.keys(compiledTemplateFileJson.Resources).forEach(key => {
-          const it = compiledTemplateFileJson.Resources[key]
-          if (it.Properties && it.Properties.Principal)
-            it.Properties.Principal = JSON.parse(
-              JSON.stringify(it.Properties.Principal)
-                .replace('{"Ref":"AWS::URLSuffix"}', '"amazonaws.com"')
-            )
-        })
-        writeFileSync(compiledTemplateFileNamePath, JSON.stringify(compiledTemplateFileJson, null, 2))
       }
     }
   }
